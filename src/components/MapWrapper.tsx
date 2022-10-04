f5425d6edfc2407dec1725472c5440b3aede6bdc
Ward <wardmaes@users.noreply.github.com>
2022-10-04 17:12:16 +0200

    Add InfoWindow on hover

--- src/components/MapWrapper.tsx
+++ src/components/MapWrapper.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react'
+import ReactDOMServer from 'react-dom/server'
 import { useActor } from '@xstate/react'
 import { Wrapper, Status } from '@googlemaps/react-wrapper'
 import { isLatLngLiteral } from '@googlemaps/typescript-guards'
@@ -90,7 +91,7 @@ export const Map: React.FC<MapProps> = ({
   return (
     <>
       <div ref={ref} style={style} />
-      {map && <DrawLayer mapProp={map} />}
+      {map && <DrawLayer mapProp={map} mapRef={ref} />}
       {React.Children.map(children, (child) => {
         if (React.isValidElement(child)) {
           // set the map prop on the child component
@@ -140,27 +141,74 @@ function useDeepCompareEffectForMaps(
 
 type DrawLayerProps = {
   mapProp: google.maps.Map
+  mapRef: any
 }
 
-export const DrawLayer = ({ mapProp }: DrawLayerProps) => {
+export const DrawLayer = ({ mapProp, mapRef }: DrawLayerProps) => {
   const [drawingManager, setDrawingManager] =
     useState<google.maps.drawing.DrawingManager>()
 
   const globalServices = useContext(GlobalStateContext)
   const [state, send] = useActor(globalServices.drawService)
 
+  function project(latLng: google.maps.LatLng) {
+    let siny = Math.sin((latLng.lat() * Math.PI) / 180)
+
+    // Truncating to 0.9999 effectively limits latitude to 89.189. This is
+    // about a third of a tile past the edge of the world tile.
+    siny = Math.min(Math.max(siny, -0.9999), 0.9999)
+
+    return new google.maps.Point(
+      256 * (0.5 + latLng.lng() / 360),
+      256 * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI))
+    )
+  }
+
+  function createInfoWindowContent(latLng: google.maps.LatLng, zoom: number) {
+    const scale = 1 << zoom
+
+    const worldCoordinate = project(latLng)
+
+    const pixelCoordinate = new google.maps.Point(
+      Math.floor(worldCoordinate.x * scale),
+      Math.floor(worldCoordinate.y * scale)
+    )
+
+    const tileCoordinate = new google.maps.Point(
+      Math.floor((worldCoordinate.x * scale) / 256),
+      Math.floor((worldCoordinate.y * scale) / 256)
+    )
+
+    return [
+      'LatLng: ' + latLng,
+      'Zoom level: ' + zoom,
+      'World Coordinate: ' + worldCoordinate,
+      'Pixel Coordinate: ' + pixelCoordinate,
+      'Tile Coordinate: ' + tileCoordinate,
+      // ReactDOMServer.renderToString(<Controls />),
+    ].join('<br>')
+  }
+
   const mouseoverListener = (
     e: google.maps.MapMouseEvent,
     polyline: google.maps.Polyline
   ) => {
     const { latLng } = e
-    if (!latLng) {
+    if (!latLng || !state.context.map) {
       return
     }
     if (google.maps.geometry.poly.isLocationOnEdge(latLng, polyline, 10e-1)) {
       console.log(
         globalServices.drawService.getSnapshot().context.existingPaths.length
       )
+
+      // TODO: convert to custom popup
+      const coordInfoWindow = new google.maps.InfoWindow()
+      coordInfoWindow.setContent(
+        createInfoWindowContent(latLng, state.context.map.getZoom()!)
+      )
+      coordInfoWindow.setPosition(latLng)
+      coordInfoWindow.open(state.context.map)
     }
   }
 
